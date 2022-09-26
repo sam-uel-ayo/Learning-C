6d59b76aa6e576d6a4ee091a268a3007bc1db900
sam-uel-ayo <samuelayomide0705@gmail.com>
2022-09-26 12:32:20 +0100

    Print heading above temperature conversion table

--- Chapter_1/Ex_1.03.c
+++ Chapter_1/Ex_1.03.c
@@ -3,18 +3,18 @@
 int main(void)
 
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
     upper = 300;
     step = 20;
-printf("fahrehiet \tcelsius\n");
+
     fahr = lower;
     while (fahr <= upper)
     {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t\t%d\n", fahr, celsius);
+        celsius = (5 / 9) * (fahr-32);
+        printf("%f %f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
